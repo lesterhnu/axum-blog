d09182750911ede149c4dddde4feebd4a027b42d
lesterhnu <lesterhnu@gmail.com>
2024-07-31 23:02:07 +0800

    update admin pages

--- src/app/handler/admin.rs
+++ src/app/handler/admin.rs
@@ -1,28 +1,29 @@
+use crate::{dto, Result,dao};
 use askama_axum::Template;
 use axum::http::HeaderMap;
-use axum::{http::StatusCode, response::Redirect};
+use axum::http::StatusCode;
 use axum::Form;
-use crate::{dto, Result};
 
-#[derive(Debug,Template)]
+#[derive(Debug, Template)]
 #[template(path = "backend/login.html")]
 pub struct LoginTemplate;
 
 pub async fn login_ui() -> Result<LoginTemplate> {
     Ok(LoginTemplate {})
 }
-pub async fn login(Form(req):Form<dto::admin::LoginReq>)->Result<(StatusCode,HeaderMap)>{
-    tracing::info!("{:#?}",req);
+pub async fn login(Form(req): Form<dto::admin::LoginReq>) -> Result<(StatusCode, HeaderMap)> {
+    tracing::info!("{:#?}", req);
     super::redirect_with_cookie()
     // Redirect::to("/admin/index")
 }
 
-
-#[derive(Debug,Template)]
+#[derive(Debug, Template)]
 #[template(path = "backend/index.html")]
-pub struct AdminIndexTemplate;
+pub struct AdminIndexTemplate{
+    pub posts:Vec<dto::posts::PostsWithTag>
+}
 
-
-pub async fn index()->Result<AdminIndexTemplate>{
-    Ok(AdminIndexTemplate{})
+pub async fn index() -> Result<AdminIndexTemplate> {
+    let posts = dao::posts::get_posts().await?;
+    Ok(AdminIndexTemplate {posts})
 }
